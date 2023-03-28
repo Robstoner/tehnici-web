92beb47a65ced4dd697a219290145ee40dc634af
Schmidt Robert <robstoner19@gmail.com>
2023-03-28 15:39:59 +0300

    Curs + Lab 6

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const fs = require("fs");
 const sass = require("sass");
+const path = require("path");
 
 obGlobal = {
   obErori: null,
@@ -10,55 +11,47 @@ obGlobal = {
 app = express();
 
 console.log("Folder proiect: " + __dirname);
+console.log("Cale fisier: " + __filename);
+console.log("Folder curent: " + process.cwd());
+
+foldere = ["temp", "temp1"];
+
+foldere.forEach((folder) => {
+  // let cale = __dirname + "/" + folder;
+  let cale = path.join(__dirname, folder);
+  if (!fs.existsSync(cale)) {
+    fs.mkdirSync(cale);
+  }
+});
 
 app.set("view engine", "ejs");
 
-app.use("/resurse", express.static(__dirname + "/resurse"));
-
-app.get("/*", (req, res) => {
-  res.render("pagini" + req.url, (err, html) => {
-    if (err) {
-      if (err.message.includes("Failed to lookup view")) {
-        afisEroare(res, "404");
-      } else {
-        afisEroare(res);
-      }
-    } else {
-      res.send(html);
-    }
-  });
-});
-
-app.get("/ceva", (req, res) => {
-  res.send("altceva");
-});
-
-app.get(["/index", "/home"], (req, res) => {
-  res.render("pagini/index");
-});
-
-app.get("/despre", (req, res) => {
-  res.render("pagini/despre");
-});
+app.use("/resurse", express.static(path.join(__dirname, "resurse")));
 
 function initErori() {
   var continut = fs
-    .readFileSync(__dirname + "/resurse/json/erori.json")
+    .readFileSync(path.join(__dirname, "resurse/json/erori.json"))
     .toString("utf-8");
   obGlobal.obErori = JSON.parse(continut);
   obGlobal.obErori.info_erori.forEach((eroare) => {
-    eroare.imagine = "/" + obGlobal.obErori.cale_baza + "/" + eroare.imagine;
+    eroare.imagine = path.join(obGlobal.obErori.cale_baza, eroare.imagine);
   });
 }
 
 initErori();
 
-function afisEroare(res, _identificator, _titlu, _text, _imagine) {
+function afisEroare(
+  res,
+  _identificator,
+  _titlu = "titlu default",
+  _text,
+  _imagine
+) {
   let vErori = obGlobal.obErori.info_erori;
   let eroare = vErori.find((eroare) => eroare.identificator == _identificator);
-
+  console.log(_identificator, eroare);
   if (eroare) {
-    let titlu1 = _titlu || eroare.titlu;
+    let titlu1 = _titlu == "titlu default" ? eroare.titlu || _titlu : _titlu;
     let text1 = _text || eroare.text;
     let imagine1 = _imagine || eroare.imagine;
 
@@ -72,6 +65,51 @@ function afisEroare(res, _identificator, _titlu, _text, _imagine) {
     res.render("pagini/eroare", obGlobal.eroare_default);
   }
 }
+
+app.use(/^\/resurse(\/[a-zA-Z0-9]*(?!\.)[a-zA-Z0-9]*)*$/, (req, res) => {
+  console.log(
+    "==========================================================================="
+  );
+  afisEroare(res, "403");
+});
+
+app.get("/favicon.ico", (req, res) => {
+  res.sendFile("favicon.ico", { root: __dirname + "/resurse/ico" });
+});
+
+app.get(["/", "/index", "/home"], (req, res) => {
+  res.render("pagini/index", { ip: req.ip });
+});
+
+// app.get("/despre", (req, res) => {
+//   res.render("pagini/despre");
+// });
+
+app.get(/\.ejs$/, (req, res) => {
+  afisEroare(res, "400");
+});
+
+app.get("/*", (req, res) => {
+  try {
+    console.log("cv");
+    res.render("pagini" + req.url, (err, html) => {
+      if (err) {
+        if (err.message.includes("Failed to lookup view")) {
+          afisEroare(res, "404");
+        } else {
+          afisEroare(res);
+        }
+      } else {
+        res.send(html);
+      }
+    });
+  } catch (err) {
+    if (err.message.includes("Cannot find module")) {
+      afisEroare(res, "404");
+    }
+  }
+});
+
 app.listen(3000, () => {
   console.log("Server started on port 3000");
 });
