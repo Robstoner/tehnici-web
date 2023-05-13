e7bc3eca85b14d41950cef9b642bcfc4a654c18f
Schmidt Robert <robstoner19@gmail.com>
2023-05-13 23:33:33 +0300

    Cleaned up code

--- index.js
+++ index.js
@@ -75,38 +75,9 @@ app.get("/galerie", (req, res) => {
   });
 });
 
-// app.get("*/galerie-animata.css", function (req, res) {
-//   var sirScss = fs
-//     .readFileSync(__dirname + "/resurse/scss_ejs/galerie-animata.scss")
-//     .toString("utf8");
-//   var culori = ["navy", "black", "purple", "grey"];
-//   var indiceAleator = Math.floor(Math.random() * culori.length);
-//   var culoareAleatoare = culori[indiceAleator];
-//   rezScss = ejs.render(sirScss, { culoare: culoareAleatoare });
-//   console.log(rezScss);
-//   var caleScss = __dirname + "/temp/galerie-animata.scss";
-//   fs.writeFileSync(caleScss, rezScss);
-//   try {
-//     rezCompilare = sass.compile(caleScss, { sourceMap: true });
-
-//     var caleCss = __dirname + "/temp/galerie-animata.css";
-//     fs.writeFileSync(caleCss, rezCompilare.css);
-//     res.setHeader("Content-Type", "text/css");
-//     res.sendFile(caleCss);
-//   } catch (err) {
-//     console.log(err);
-//     res.send("Eroare");
-//   }
-// });
-
-// app.get("*/galerie-animata.css.map", function (req, res) {
-//   res.sendFile(path.join(__dirname, "temp/galerie-animata.css.map"));
-// });
-
 app.get("/*", (req, res) => {
   try {
     res.render("pagini" + req.url, (err, html) => {
-      console.log(err);
       if (err) {
         if (err.message.includes("Failed to lookup view")) {
           afisEroare(res, "404");
@@ -167,7 +138,9 @@ function initErori() {
   var continut = fs
     .readFileSync(path.join(__dirname, "resurse/json/erori.json"))
     .toString("utf-8");
+
   obGlobal.obErori = JSON.parse(continut);
+
   obGlobal.obErori.info_erori.forEach((eroare) => {
     eroare.imagine =
       "/" + path.join(obGlobal.obErori.cale_baza, eroare.imagine);
@@ -192,7 +165,7 @@ function afisEroare(
     let imagine1 = _imagine || eroare.imagine;
 
     if (eroare.status) res.status(eroare.identificator);
-    console.log(titlu1, text1, imagine1);
+
     res.render("pagini/eroare", {
       titlu: titlu1,
       text: text1,
